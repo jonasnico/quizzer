6d4a677e5015998f67ea7f247d69fe26f80666b4
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-06-21 11:09:48 +0200

    fix: formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://jonasnico.github.io',
-  base: '/quizzer',
+  site: "https://jonasnico.github.io",
+  base: "/quizzer",
   vite: {
     plugins: [tailwindcss()],
   },
